a47720ff2767b80b22c5f1079711bca54bdb5106
Graham <grahamspence@gmail.com>
2018-07-24 16:44:36 -0400

    book lending exercise complete

--- book-lending.rb
+++ book-lending.rb
@@ -75,7 +75,7 @@ puts sister_outsider.borrow # false
 puts sister_outsider.due_date # 2017-02-25 20:52:20 -0500 (this value will be different for you)
 puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
 puts Book.borrowed.inspect # [#<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=2017-02-25 20:55:17 -0500>]
-puts Book.overdue.inspect # []
+puts Book.overdue_books.inspect # []
 puts sister_outsider.return_to_library # true
 puts sister_outsider.lent_out? # false
 puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
